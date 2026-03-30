1dc104bf12af08454968d610f4fc657f42507622
Виктор <viktor.dubov.2007@gmail.com>
2026-03-30 17:06:08 +0300

    Fix

---
+++ README.md
@@ -0,0 +1,13 @@
+# LMS Project (Django + DRF + Celery + Stripe + Docker)
+
+## 🚀 Запуск проекта
+
+### 1. Установить Docker
+Установите Docker Desktop:
+https://www.docker.com/products/docker-desktop/
+
+### 2. Клонировать репозиторий
+
+```bash
+git clone <repo_url>
+cd Documentation_and_Security
--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # -----------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / os.getenv("DATABASE_NAME", "db.sqlite3"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
